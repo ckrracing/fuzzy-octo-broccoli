4375579b0f870b0e5e50dcd12616ebca41dd7e92
ckrracing <netadmin@thisdigitalife.net>
2017-05-15 21:13:46 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
+class View {
+  public name = "bart" ;
+}
+
+$View = new View(); 
+
 ?>
+<html>
+  
+  <head></head>
+  
+   <body>
+    <H1> <?php $View->name ?> </H1>
+  </body>
+
+</html>
